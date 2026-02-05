af00dd5fc8fc12e4ebce4e17c344732eaddf978e
JiyalPatel <jiyalpatel10@gmail.com>
2026-02-05 13:05:17 +0530

    some more giggles

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ export const CONTACT_INFO = {
 };
 
 export const WARNING_CONFIG = {
-    text: "🔔 IMPORTANT NOTICE: Registrations for The One Quest (Treasure Hunt) are now CLOSED. LAST 15 Minutes to Register...... Close at 1 : 15",
+    text: "🔔 IMPORTANT NOTICE: Registrations for The One Quest (Treasure Hunt) are now CLOSED. LAST 15 Minutes to Register...... Close at 1:15 PM",
 };
 
 /* --- CUSTOM STYLES (styles.css equivalent) --- */
